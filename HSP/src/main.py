d8eecf14b78463a430497254f49f3c7f3ca85cfe
Yang1996 <32739399+LucyYangfighting@users.noreply.github.com>
2018-10-06 13:53:58 +0800

    Update main.py

--- HSP/src/main.py
+++ HSP/src/main.py
@@ -3,45 +3,54 @@ from sklearn import preprocessing
 '''
 以下都是input
 '''
-dim = 2
-num_elements = 6
-data = np.array([[3, 1], [-2, -1], [-3, -2], [-1, -1], [2, 1], [3, 2]])
+dim=30
+num_elements=74902
 degree=[]#这里要得到知识图谱中每个实体的度（出入都包括,一般是同类型之间的度），作为input，自行计算
 file_path=''
 
-def preprocessing():
+def preprocessing_data():
     # 归一化处理,使用的是z-scores
     # scaler = preprocessing.StandardScaler()
     # data_scaled = scaler.fit_transform(data)
-    #归一化处理，使用的是min-max,(0,1)
+    #归一化处理，使用的是min-max
+    #input
+    datalist = []
+    f = open('/Users/yangqingqing/Documents/Entity_Profiling/structural/1858106.txt', "r")
+    lines = f.readlines()
+    for line in lines:
+        m = line.strip().split(' ')
+        l = m[1:]
+        datalist.append(l)
+    data = np.array(datalist)
+
     min_max_scaler = preprocessing.MinMaxScaler()
     data_scaled = min_max_scaler.fit_transform(data)
+
     return data_scaled
 
-def get_r():
+def get_r(data_scaled):
     '''
     归一化后计算平均距离和hyperparameter半棱长r
     :return: r
     '''
-    data_scaled=preprocessing()
-    #找平均距离和半棱长r
     J=[]#平均距离
-    m, n = data_scaled.shape#array的行和列，n=num_elements，m=dim
-    for i in range(n):
+    for i in range(dim):
         data_column=data_scaled[:,i]
+        data_column.sort()
         sum=0
-        for j in range(1,len(data_column)):
+        for j in range(1,num_elements):
             sum+=data_column[j]-data_column[j-1]
-        J[i]=sum/(num_elements-1)
+        J.append(sum/(num_elements-1))
     s=1
     for i in J:
         s=s * i
-    r=pow(s,1/dim)#开根号得到r
+    r=pow(s,1/dim)
     return r
 
-def change_r():
+def change_r(r):
     '''
-    对r进行缩放
+    传入原始的半径r
+    :param r:
     :return:
     '''
     sum_d=0
@@ -51,26 +60,22 @@ def change_r():
     d_mean=sum_d/num_elements
     w=d_mean/pow(2,dim)
     #缩放后的r_new
-    r_new=pow(w,1/dim)*get_r()
+    r_new=pow(w,1/dim)*r
 
     return r_new
 
-def find_neighbor(data_scaled):
+def find_neighbor(data_scaled,cr):
     '''
     得到hypercube中最近邻节点
     :param data_scaled:
+    :param cr:
     :return:
     '''
-    cr=change_r()
     #得到每维特征排序后的data_array，方便后续查找数据，先排序，复杂度是O(dim*n*logn),
     data_sorted = np.sort(data_scaled, axis=0)  # 按列排序
     index_sorted = np.argsort(data_scaled, axis=0)  # 按列排序的到变化后的索引
     #排序之后顺序换了样本顺序换了，需要保存下来故有了index_sorted
-    '''
-    二分查找得到区间(f-r，f+r)的样本,只是要找到区间中的值，二分找而不是从前往后O(n)
-    '''
     result=[]
-
     for i in range(num_elements):
         #求第i个样本点的近邻点
         res_array=[]
@@ -87,13 +92,21 @@ def find_neighbor(data_scaled):
         s=set()
         for re in res_array:
             s=s.intersection(set(re))
-        result.append(l+list(s))
 
-    return result
+        m=l+list(s)
+        print(m)
+        result.append(m)
+        # with open(file_path, "a") as f:
+        #     for re in m:
+        #         s = str(re) + ' '
+        #     s += '\n'
+        #     f.writelines(s)
+        # f.close()
+    write_txt(result)
 
-
-def BinarySearch_interval(list,list_Index,min,max):
+def BinarySearch_interval(list1,list_Index,min,max):
     '''
+    二分查找得到区间(f-r，f+r)的样本,只是要找到区间中的值，二分找而不是从前往后O(n)
     找出该维度特征上对应区间的（min,max）的点，返回这些点（点最原始的索引值）
     :param list:
     :param list_Index:
@@ -103,26 +116,28 @@ def BinarySearch_interval(list,list_Index,min,max):
     '''
     res=[]
     start=0
-    end=len(list)
+    end=len(list1)
     while start<=end:
-        mid=(start+end)/2
-        if list[mid]>=min and list[mid]<=max:
+        mid=int((start+end)/2)
+        if list1[mid]>=min and list1[mid]<=max:
             break
-        if list[mid]<min:
+        if list1[mid]<min:
             start=mid+1
-        if list[mid]>max:
+        if list1[mid]>max:
             end=mid-1
 
     i=mid
     j=mid
-    while list[i]>=min and list[i]<=max:
+    while list1[i]>=min and list1[i]<=max:
         res.append(list_Index[i])
         i=i-1
 
-    while list[j]>=min and list[j]<= max:
+    while list1[j]>=min and list1[j]<= max:
         res.append(list_Index[j])
         j=j+1
+
     return res
+
 
 def write_txt(list):
     with open(file_path,"a") as f:
@@ -135,11 +150,20 @@ def write_txt(list):
     f.close()
 
 def main():
-    preprocessing()
+    data_scaled=preprocessing_data()
+    change=False
+
+    if change:
+        tempr = get_r(data_scaled)
+        r = change_r(tempr)
+    else:
+        r = get_r(data_scaled)
+
+    #print(r) #1.3350956596040108e-05
+
     #将结果写入文件中
-    write_txt(find_neighbor())
+    find_neighbor(data_scaled,r)
+
 
 if __name__ == '__main__':
  main()
-
-
